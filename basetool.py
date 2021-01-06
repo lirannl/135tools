baf1b6419c2ecae5cf8a4f53695875409df14737
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2021-01-06 21:09:06 +1000

    Frac'ing renaming!
    9 nc7oq2Z1zfoax+qAIoT3B5e5kBFhHLlE8b0wWtgW/yltOWiOilaqhQ1cc8atTaTIHxL8EHt0K8SE=NtVm40Qh4BC9DeVyOtOW78oOKwujQNYaFOHqrtUcILYTr=6K6cSeXjDzHo/aqlwYyLvUF7Zd=AxBJ27BcQDmulPgWdv7dvZWm1wohVwmA=ylibxPAX55SfxoNVrsyww1Fileypmd7/

--- basetool.py
+++ basetool.py
@@ -4,27 +4,25 @@ from math import pow
 from re import search
 #Default character set.
 charSet = "".join([
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
-    'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '+', '/', '~', '!', '@', '#', 
-    '$', '%', '^', '&', '*', ';', '=', '?', '<', '>', '[', ']', ':', '"', '{', '}', ',', 
-    '`'])
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '+', '/', '~', '!', '@', '#', '$', '%', '^', '&', 
+    '*', ';', '=', '?', '<', '>', '[', ']', ':', '"', '{', '}', ',', '`'])
 #Check if inputSet is set.
 def isSet(inputSet):
-    if len(inputSet) == len(set(inputSet)): return True
-    return False
+    return len(inputSet) == len(set(inputSet))
 #Check for bad inputs.
-def valuesCheck(inputBase, outputBase, inputSet, outputSet, charSet, decimals):
-    if inputBase < 0 or inputBase > len(charSet):
+def valuesCheck(inputBase, outputBase, inputSet, outputSet, charSet, fracPlaces):
+    if inputBase < 2 or inputBase > len(charSet):
         raise ValueError("Input base is out of range")
     if outputBase < 0 or outputBase > len(charSet):
         raise ValueError("Output base is out of range")
-    if (isSet(inputSet) or isSet(outputSet)) is False:
+    if not (isSet(inputSet) or not isSet(outputSet)):
         raise ValueError("Custom set is not unique")
     if search("[.-]", inputSet + outputSet) is not None:
         raise ValueError("Custom set contains - or .")
-    if decimals < 0 or decimals > 24:
+    if fracPlaces < 0 or fracPlaces > 24:
         raise ValueError("Number decimals is out of range")
 #Trim inputSet to base.
 def trimSet(inputSet, inputBase):
@@ -43,31 +41,29 @@ def inputPosition(absInString, inCutSet):
     if -1 in inputPosList: raise ValueError("Input characters not in input set")
     return inputPosList
 #Split decimal inputs.
-def inputDecSplit(inputSet, inputPosList):
+def inputFloatSplit(inputSet, inputPosList):
     if (len(inputSet) - 1) in inputPosList: 
         intPosList = inputPosList[inputPosList.index(len(inputSet)-1) + 1:]
-        decPosList = reversed(inputPosList[:inputPosList.index(len(inputSet)-1)])
-        return intPosList, decPosList, True
-    intPosList = inputPosList
-    return intPosList, [], False
+        fracPosList = reversed(inputPosList[:inputPosList.index(len(inputSet)-1)])
+        return intPosList, fracPosList, True
+    return inputPosList, [], False
 #Convert number to Base10.
 def convertDecimal(absInString, inputBase, inCutSet, inputPosList):
-    if inCutSet == ["0123456789."]: return absInString
-    intPosList, decPosList, floatInput = inputDecSplit(inCutSet, inputPosList)
-    intList = map(lambda pos:(inputBase ** pos[0])*pos[1], enumerate(intPosList))
+    if inCutSet == ["0123456789."]: return float(absInString)
+    intPosList, fracPosList, floatInput = inputFloatSplit(inCutSet, inputPosList)
+    intList = map(lambda pos:(inputBase**pos[0])*pos[1], enumerate(intPosList))
     if not floatInput: return sum(intList)
-    decList = map(lambda pos:(inputBase ** ((pos[0]*-1)-1))*pos[1], enumerate(decPosList))
-    return (sum(intList) + sum(decList))
+    fracList = map(lambda pos:(inputBase**((pos[0]*-1)-1))*pos[1], enumerate(fracPosList))
+    return (sum(intList) + sum(fracList))
 #Convert base of number.
 def baseConvert(inputString:str, inputBase:int, outputBase:int = 10,
-                inputSet:str = charSet, outputSet:str = charSet, decimals:int = 5 ):
-    valuesCheck(inputBase, outputBase, inputSet, outputSet, charSet, decimals)
+                inputSet:str = charSet, outputSet:str = charSet, fracPlaces:int = 5 ):
+    valuesCheck(inputBase, outputBase, inputSet, outputSet, charSet, fracPlaces)
     inCutSet, outCutSet = trimSet(inputSet, inputBase), trimSet(outputSet, outputBase)
     inString = inputFlatten(inputString, inputBase, inputSet, charSet)
     absInString, sign = inputSign(inString)
     inputPosList = inputPosition(absInString, inCutSet)
-    if outputBase == 0: return 42
-    decInString = convertDecimal(absInString, inputBase, inCutSet, inputPosList)
-    return decInString
+    fracInString = convertDecimal(absInString, inputBase, inCutSet, inputPosList)
+    return fracInString
 
-print(str(baseConvert("101011.11", 2, 10, decimals = 20)))
+print(str(baseConvert("101011.11", 2, 10, fracPlaces = 20)))
