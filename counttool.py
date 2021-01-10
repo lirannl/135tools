e639e8d7915e3eaccda7f7c198d6b79e725bc00a
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2021-01-10 15:56:06 +1000

    Update counttool.py
    Added support for flattening letters and ignoring spaces.

--- counttool.py
+++ counttool.py
@@ -1,14 +1,20 @@
 #Tool to analyse the frequency of each character in a data set.
 #Created date: 09/01/2021
+#Format spaces and capitals in input.
+def stringFormat(inputString, spaces, capitals):
+    if spaces: spaceString = inputString
+    else: spaceString = inputString.replace(" ", "")
+    if capitals: return spaceString, len(spaceString)
+    return spaceString.lower(), len(spaceString)
 #Key to order the character dictionary.
 def charOrder(inPos):
     _, charCount, charPercent = inPos
     return charCount, charPercent
 #Analyse input for character occurrences.
-def charAnalysis(inputString:str):
-    length = len(inputString)
-    charSet = list(set(inputString))
-    countList = list(map(lambda pos:inputString.count(pos), charSet))
+def charAnalysis(inputString:str, spaces:bool = True, capitals:bool = True):
+    inString, length = stringFormat(inputString, spaces, capitals)
+    charSet = list(set(inString))
+    countList = list(map(lambda pos:inString.count(pos), charSet))
     percentList = list(map(lambda pos:round((int(pos)/length)*100, 3), countList))
     charDict = zip(charSet, countList, percentList)
     return sorted(charDict, key=charOrder, reverse=True)
