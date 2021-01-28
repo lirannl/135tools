1c47d6f2aa7009f0ea2f0328705774201793edcb
Liran Piade <liranpiade@gmail.com>
2021-01-28 17:21:42 +1000

    Moved the API interface to a wrapper function

--- basetool.py
+++ basetool.py
@@ -1,20 +1,24 @@
-category="tools"
-#Tool to convert int/float numbers between bases 0-86 with given character sets.
-#Created date: 05/01/2021
-from math import pow
 from re import search
-#Default character set.
+from math import pow
+category = "tools"
+# Tool to convert int/float numbers between bases 0-86 with given character sets.
+# Created date: 05/01/2021
+# Default character set.
 charSet = "".join([
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
-    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
-    'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '+', '/', '~', '!', '@', '#', 
-    '$', '%', '^', '&', '*', ';', '=', '?', '<', '>', '[', ']', ':', '"', '{', '}', ',', 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '+', '/', '~', '!', '@', '#',
+    '$', '%', '^', '&', '*', ';', '=', '?', '<', '>', '[', ']', ':', '"', '{', '}', ',',
     '`'])
-#Check if inputSet is set.
+# Check if inputSet is set.
+
+
 def isSet(inputSet):
     return len(inputSet) == len(set(inputSet))
-#Check for bad inputs.
+# Check for bad inputs.
+
+
 def valuesCheck(inBase, outBase, inputSet, outputSet, charSet, fracPlaces):
     if inBase < 2 or inBase > len(charSet):
         raise ValueError("Input base is out of range")
@@ -28,67 +32,107 @@ def valuesCheck(inBase, outBase, inputSet, outputSet, charSet, fracPlaces):
         raise ValueError("Custom set contains - or .")
     if fracPlaces < 0:
         raise ValueError("Negative decimal places invalid")
-#Trim inputSet to base.
+# Trim inputSet to base.
+
+
 def trimSet(inputSet, inBase):
     return inputSet[0:inBase] + "."
-#Flatten input if possible.
+# Flatten input if possible.
+
+
 def inputFlatten(inputString, inBase, inputSet, charSet):
-    if inBase < 36 and inputSet == charSet: return inputString.lower()
+    if inBase < 36 and inputSet == charSet:
+        return inputString.lower()
     return inputString
-#Check if input is negative.
+# Check if input is negative.
+
+
 def inputSign(inString):
-    if inString[0] == "-": return inString[1:], True
+    if inString[0] == "-":
+        return inString[1:], True
     return inString, False
-#Return list of string indexs.
+# Return list of string indexs.
+
+
 def inputPosition(absInString, inCutSet):
-    inPosList = list(map(lambda char:inCutSet.find(char), absInString))[::-1]
-    if -1 in inPosList: raise ValueError("Input characters not in input set")
+    inPosList = list(map(lambda char: inCutSet.find(char), absInString))[::-1]
+    if -1 in inPosList:
+        raise ValueError("Input characters not in input set")
     return inPosList
-#Split decimal inputs.
+# Split decimal inputs.
+
+
 def inputFloatSplit(inputSet, inPosList):
-    if (len(inputSet) - 1) in inPosList: 
+    if (len(inputSet) - 1) in inPosList:
         intPosList = inPosList[inPosList.index(len(inputSet)-1) + 1:]
         fracPosList = reversed(inPosList[:inPosList.index(len(inputSet)-1)])
         return intPosList, fracPosList, True
     return inPosList, [], False
-#Convert number to Base10.
+# Convert number to Base10.
+
+
 def convertDecimal(absInString, inBase, inCutSet, inPosList):
-    if inCutSet == ["0123456789."]: return str(absInString)
+    if inCutSet == ["0123456789."]:
+        return str(absInString)
     intPosList, fracPosList, floatInput = inputFloatSplit(inCutSet, inPosList)
-    intList = map(lambda pos:(inBase**pos[0])*pos[1], enumerate(intPosList))
-    if not floatInput: return sum(intList), False
-    fracList = map(lambda pos:(inBase**((pos[0]*-1)-1))*pos[1], enumerate(fracPosList))
+    intList = map(lambda pos: (inBase**pos[0])*pos[1], enumerate(intPosList))
+    if not floatInput:
+        return sum(intList), False
+    fracList = map(lambda pos: (
+        inBase**((pos[0]*-1)-1))*pos[1], enumerate(fracPosList))
     return str(sum(intList) + sum(fracList)), True
-#Divmod until quotient is zero.
-def inputDivmod(inputQuotient, outBase, remainder = []):
+# Divmod until quotient is zero.
+
+
+def inputDivmod(inputQuotient, outBase, remainder=[]):
     quotient, remainderList = divmod(inputQuotient, outBase)
-    if inputQuotient == 0: return remainder 
+    if inputQuotient == 0:
+        return remainder
     return inputDivmod(quotient, outBase, [int(remainderList)] + remainder)
-#Convert number to output base.
+# Convert number to output base.
+
+
 def outputPosition(fracInString, outBase, fracPlaces):
     if "." in str(fracInString):
         inputQuotient = float(fracInString) * pow(outBase, fracPlaces)
-    else: inputQuotient = int(fracInString)
+    else:
+        inputQuotient = int(fracInString)
     return inputDivmod(inputQuotient, outBase)
-#Substitute characters to set.
+# Substitute characters to set.
+
+
 def subCharacters(outPosList, outCutSet):
     return "".join(list(map(lambda pos: outCutSet[pos], outPosList)))
-#Format sign and decimal place.
+# Format sign and decimal place.
+
+
 def outputFormat(string, fracPlaces, fracInput, sign):
-    if fracInput and fracPlaces != 0: 
+    if fracInput and fracPlaces != 0:
         fracString = string[:(fracPlaces*-1)] + "." + string[(fracPlaces*-1):]
-    else: fracString = string
-    if sign: return "-" + fracString
+    else:
+        fracString = string
+    if sign:
+        return "-" + fracString
     return fracString
-#Convert base of number.
-def API_baseConvert(inputString:str, inBase:int, outBase:int = 10,
-                inputSet:str = charSet, outputSet:str = charSet, fracPlaces:int = 5 ):
+# Convert base of number.
+
+
+def baseConvert(inputString: str, inBase: int, outBase: int = 10,
+                inputSet: str = charSet, outputSet: str = charSet, fracPlaces: int = 5):
     valuesCheck(inBase, outBase, inputSet, outputSet, charSet, fracPlaces)
-    inCutSet, outCutSet = trimSet(inputSet, inBase), trimSet(outputSet, outBase)
+    inCutSet, outCutSet = trimSet(
+        inputSet, inBase), trimSet(outputSet, outBase)
     inString = inputFlatten(inputString, inBase, inputSet, charSet)
     absInString, sign = inputSign(inString)
     inPosList = inputPosition(absInString, inCutSet)
-    fracInString, fracInput = convertDecimal(absInString, inBase, inCutSet, inPosList)
+    fracInString, fracInput = convertDecimal(
+        absInString, inBase, inCutSet, inPosList)
     outPosList = outputPosition(fracInString, outBase, fracPlaces)
     outputString = subCharacters(outPosList, outCutSet)
     return outputFormat(outputString, fracPlaces, fracInput, sign)
+
+
+def API_convert(inputString: str, inputBase: str,
+                outputBase: str = "10", inputSet: str = charSet,
+                outputSet: str = charSet, fracPlaces: str = "5"):
+    return baseConvert(inputString, int(inputBase), int(outputBase), inputSet, outputSet, int(fracPlaces))
