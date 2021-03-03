2eb7357fec03750e0c82605b04dd5c5a80846f42
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2021-03-03 17:33:46 +1000

    Update to formatting and reworking advanced options.

--- counttool.py
+++ counttool.py
@@ -1,19 +1,26 @@
+########################################################################################
 #Tool to analyse and compare the frequency of each character in a data set.
 #Created date: 09/01/2021
+
+########################################################################################
+#135code.com API Category Definition.
 category = "tools"
+
 #Format spaces and capitals in input.
-def stringFormat(inputString, spaces, capitals):
-    if spaces: spaceString = inputString
-    else: spaceString = inputString.replace(" ", "")
-    if capitals: return spaceString, len(spaceString)
+def stringFormat(inputString, excludeSpaces, caseSensitive):
+    if excludeSpaces: inputString.replace(" ", "")
+    else: spaceString = spaceString = inputString
+    if caseSensitive: return spaceString, len(spaceString)
     return spaceString.lower(), len(spaceString)
+    
 #Key to order the character dictionary.
 def charOrder(inPos):
     _, charCount, charPercent = inPos
     return charCount, charPercent
+
 #Analyse input for character occurrences.
-def API_charAnalysis(inputString:str, spaces:bool = True, capitals:bool = True):
-    inString, length = stringFormat(inputString, spaces, capitals)
+def API_charAnalysis(inputString:str, excludeSpaces:bool = True, caseSensitive:bool = True):
+    inString, length = stringFormat(inputString, excludeSpaces, caseSensitive)
     charSet = list(set(inString))
     countList = list(map(lambda pos:inString.count(pos), charSet))
     percentList = list(map(lambda pos:round((int(pos)/length)*100, 3), countList))
